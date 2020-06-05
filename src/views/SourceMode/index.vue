5d57db9d0050055217663db3ab59dfd16f8a6c80
zhangxiaoyuan1221 <624884801@qq.com>
2020-06-05 15:11:45 +0800

    20200605

--- src/views/SourceMode/index.vue
+++ src/views/SourceMode/index.vue
@@ -25,12 +25,13 @@ export default {
   beforeMount() {
     var $this = this;
     if ($this.filebox.rowData != "" || $this.filebox.rowData.length > 0) {
-      this.innerPieChart($this.filebox);
-      this.linearChart($this.filebox);
+      $this.innerPieChart($this.filebox);
+      $this.linearChart($this.filebox);
     }
   },
   watch: {
     filebox: function(val) {
+      console.log(val, "val");
       if (val.rowData != "" || val.rowData.length > 0) {
         this.innerPieChart(val);
         this.linearChart(val);
@@ -40,7 +41,7 @@ export default {
   methods: {
     innerPieChart(dataBox) {
       var quantity = dataBox.rowData.length;
-      //console.log(dataBox.rowData);
+      console.log(dataBox);
       var dataobj = dataBox.rowData; //原始对象
       var arrlist = [];
       dataobj.forEach(function(item, index) {
@@ -72,20 +73,76 @@ export default {
       //console.log(newAry); //去重后的数据
       var _this = this;
       newAry.map(function(item) {
-        item.proportion = _this.changTwoDecimal(item.number / quantity);
+        item.proportion = _this.multiple(
+          _this.divide(item.number, quantity),
+          100
+        );
       });
       this.newlist = newAry;
-      //console.log(this.newlist);
+      console.log(this.newlist);
     }, //innerPieChart结束
-    changTwoDecimal: function(item) {
-      //把小数去除到小数点后面2位数
-      var f_x = parseFloat(item);
-      if (isNaN(f_x)) {
-        return false;
+    multiple(a, b) {
+      var c = 0,
+        d = a.toString(),
+        e = b.toString();
+      try {
+        c += d.split(".")[1].length;
+      } catch (f) {}
+      try {
+        c += e.split(".")[1].length;
+      } catch (f) {}
+      return (
+        (Number(d.replace(".", "")) * Number(e.replace(".", ""))) /
+        Math.pow(10, c)
+      );
+    },
+    divide(arg1, arg2) {
+      var d1,
+        d2,
+        n1 = Number(arg1.toString().replace(".", "")),
+        n2 = Number(arg2.toString().replace(".", ""));
+      try {
+        d1 = arg1.toString().split(".")[1].length;
+      } catch (e) {
+        d1 = 0;
       }
-      f_x = Math.round(f_x * 10000) / 10000;
-      return f_x;
-    }, //changTwoDecimal结束
+      try {
+        d2 = arg2.toString().split(".")[1].length;
+      } catch (e) {
+        d2 = 0;
+      }
+      return this.toFixed((n1 / n2) * Math.pow(10, d2 - d1), 4);
+    },
+    toFixed(arg, n) {
+      if (n == 0) {
+        return Math.round(arg);
+      }
+      try {
+        var d,
+          carryD,
+          i,
+          ds = arg.toString().split("."),
+          len = ds[1].length,
+          b0 = "",
+          k = 0;
+        if (len > n) {
+          while (k < n && ds[1].substring(0, ++k) == "0") {
+            b0 += "0";
+          }
+          d = Number(ds[1].substring(0, n));
+          carryD = Math.round(Number("0." + ds[1].substring(n, len)));
+          len = (d + carryD).toString().length;
+          if (len > n) {
+            return Number(ds[0]) + 1;
+          } else if (len == n) {
+            return Number(ds[0] + "." + (d + carryD));
+          }
+          return Number(ds[0] + "." + b0 + (d + carryD));
+        }
+      } catch (e) {}
+      return arg;
+    },
+    //changTwoDecimal结束
     linearChart(dataBox) {
       var dataobj = dataBox.rowData; //原始对象
       var datalist = [];
@@ -113,19 +170,8 @@ export default {
           }
         });
       });
-      // console.log(haslist); //得出设备和数量时间段
 
-      //比较两个时间大小来进行排序
-      let soltTime = function(str) {
-        return new Date(str.replace(/-/g, "/")).getTime(); //用/替换日期中的-是为了解决Safari的兼容
-      };
-      for (let i = 0; i < haslist.length; i++) {
-        haslist[i].timeNew = soltTime(haslist[i].time);
-      }
-      haslist.sort(function(a, b) {
-        return b.timeNew < a.timeNew ? 1 : -1;
-      });
-      //console.log(haslist); //时间排序后的数组
+      //   console.log(haslist); //得出设备和数量时间段
       var stillist = [];
       //去除多余的属性得出最终的想要的
       haslist.forEach(function(item) {
@@ -133,56 +179,16 @@ export default {
         arrTimes.name = item.device;
         arrTimes.time = item.time;
         arrTimes.number = item.number;
-
         stillist.push(arrTimes);
       });
-      //console.log(stillist);
-      //   this.arrlist = stillist;
-      // console.log(this.arrlist);
-      var lenghtindex = this.dayTiem(
-        stillist[0].time,
-        stillist[stillist.length - 1].time
-      ); //得出最大和最小时间相差的天数
-      var newTime = [];
-      newTime.push(stillist[0].time);
-
-      for (var i = 0; i < lenghtindex; i++) {
-        newTime.push(this.getNewDay(newTime[i], 1));
-      }
-      //console.log(newTime); //得出的數組時間天數
-      //console.log(this.screenlist(stillist));
-      //console.log(stillist);
 
       this.arrlist = this.sortlist(
         stillist,
         this.screenlist(stillist),
-        newTime
+        dataBox.timeDate
       );
-      //console.log(this.arrlist);
     },
-    //两个时间相减得出的天数
-    dayTiem: function(date1, date2) {
-      //date1:小日期   date2:大日期
-      var sdate = new Date(date1);
-      var now = new Date(date2);
-      var days = now.getTime() - sdate.getTime();
-      var day = parseInt(days / (1000 * 60 * 60 * 24));
-      return day;
-    }, //dayTiem完结
-    //日期加上天数得到新的日期
-    //dateTemp 需要参加计算的日期，days要添加的天数，返回新的日期，日期格式：YYYY-MM-DD
-    getNewDay(dateTemp, days) {
-      var dateTemp = dateTemp.split("-");
-      var nDate = new Date(dateTemp[1] + "-" + dateTemp[2] + "-" + dateTemp[0]); //转换为MM-DD-YYYY格式
-      var millSeconds = Math.abs(nDate) + days * 24 * 60 * 60 * 1000;
-      var rDate = new Date(millSeconds);
-      var year = rDate.getFullYear();
-      var month = rDate.getMonth() + 1;
-      if (month < 10) month = "0" + month;
-      var date = rDate.getDate();
-      if (date < 10) date = "0" + date;
-      return year + "-" + month + "-" + date;
-    },
+
     // 排序列表并补齐缺失数据
     sortlist: function(element, list, timeList) {
       var typeList = timeList;
